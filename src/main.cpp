f25b8bfe75e967d965c564e55575ec4e8f9f7410
FyTal0n <1800013071@pki.edu.cn>
2021-06-17 18:00:32 +0800

    "part23"

--- src/main.cpp
+++ src/main.cpp
@@ -18,13 +18,26 @@ int main(int argc, char *argv[]){
     Troot->generate_ir();
     fclose(Tigger);
     fclose(zzin);*/
+
     /*zzin = fopen(argv[3], "r");
     Tigger = fopen(argv[5], "w");
     zzparse();
     Troot->generate_ir();
     fclose(Tigger);
     fclose(zzin);*/
-    rrin = fopen(argv[2], "r");
+
+    /*rrin = fopen(argv[2], "r");
+    RISCV = fopen(argv[4], "w");
+    rrparse();
+    fclose(rrin);*/
+
+    zzin = fopen(argv[2], "r");
+    Tigger = fopen("m.tigger", "w");
+    zzparse();
+    Troot->generate_ir();
+    fclose(Tigger);
+    fclose(zzin);
+    rrin = fopen("m.tigger", "r");
     RISCV = fopen(argv[4], "w");
     rrparse();
     fclose(rrin);
