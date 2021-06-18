905b1ab542fb13fce3272240326e5a2232516ca6
FyTal0n <1800013071@pki.edu.cn>
2021-06-18 09:33:33 +0800

    "part1"

--- src/main.cpp
+++ src/main.cpp
@@ -6,18 +6,19 @@ extern FILE *zzin, *yyin, *rrin;
 FILE* RISCV;
 
 int main(int argc, char *argv[]){
-    yyin = fopen(argv[2], "r");
+    yyin = fopen(argv[3], "r");
     Eeyore = fopen("m.eeyore", "w");
+    Eeyore = fopen(argv[5], "w");
     yyparse();
     root->generate_ir();
     fclose(Eeyore);
     fclose(yyin);
-    zzin = fopen("m.eeyore", "r");
+    /*zzin = fopen("m.eeyore", "r");
     Tigger = fopen("m.tigger", "w");
     zzparse();
     Troot->generate_ir();
     fclose(Tigger);
-    fclose(zzin);
+    fclose(zzin);*/s
 
     /*zzin = fopen(argv[3], "r");
     Tigger = fopen(argv[5], "w");
@@ -37,9 +38,9 @@ int main(int argc, char *argv[]){
     Troot->generate_ir();
     fclose(Tigger);
     fclose(zzin);*/
-    rrin = fopen("m.tigger", "r");
+    /*rrin = fopen("m.tigger", "r");
     RISCV = fopen(argv[4], "w");
     rrparse();
     fclose(rrin);
-    fclose(RISCV);
+    fclose(RISCV);*/
 }
