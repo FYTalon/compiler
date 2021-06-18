fd8c3d1d6bf6d22dc16bf53725e738621a059bff
FyTal0n <1800013071@pki.edu.cn>
2021-06-18 09:46:03 +0800

    "part12"

--- src/main.cpp
+++ src/main.cpp
@@ -7,40 +7,22 @@ FILE* RISCV;
 
 int main(int argc, char *argv[]){
     yyin = fopen(argv[3], "r");
-    //Eeyore = fopen("m.eeyore", "w");
-    Eeyore = fopen(argv[5], "w");
+    Eeyore = fopen("m.eeyore", "w");
+    //Eeyore = fopen(argv[5], "w");
     yyparse();
     root->generate_ir();
     fclose(Eeyore);
     fclose(yyin);
-    /*zzin = fopen("m.eeyore", "r");
-    Tigger = fopen("m.tigger", "w");
-    zzparse();
-    Troot->generate_ir();
-    fclose(Tigger);
-    fclose(zzin);*/
 
-    /*zzin = fopen(argv[3], "r");
+    zzin = fopen("m.eeyore", "r");
     Tigger = fopen(argv[5], "w");
     zzparse();
     Troot->generate_ir();
     fclose(Tigger);
-    fclose(zzin);*/
+    fclose(zzin);
 
     /*rrin = fopen(argv[2], "r");
     RISCV = fopen(argv[4], "w");
     rrparse();
     fclose(rrin);*/
-
-    /*zzin = fopen(argv[2], "r");
-    Tigger = fopen("m.tigger", "w");
-    zzparse();
-    Troot->generate_ir();
-    fclose(Tigger);
-    fclose(zzin);*/
-    /*rrin = fopen("m.tigger", "r");
-    RISCV = fopen(argv[4], "w");
-    rrparse();
-    fclose(rrin);
-    fclose(RISCV);*/
 }
